2ade571e91bf283b26a062b2a669bf38d22ae0ee
Jackson Henry <jackson.henry3@gmail.com>
2023-11-27 16:34:43 -0500

    new orders

--- src/orders.py
+++ src/orders.py
@@ -1,12 +1,13 @@
 import numpy as np
+import numpy.random
 
 rng = np.random.default_rng()
 
 
-def n_random_line_orders(num_qbits: int, chunk_sizes: list[int], n: int) -> list[np.ndarray]:
+def n_random_c6_orders(num_qbits: int, chunk_sizes: list[int], n: int) -> list[np.ndarray]:
     assert sum(chunk_sizes) == num_qbits, "chunk sizes must add up to the number of qbits"
     split_indices = np.cumsum(chunk_sizes)[:-1]
-    return [np.split(np.roll(np.arange(num_qbits), np.random.random_integers(num_qbits)), split_indices) for _ in range(n)]
+    return [np.split(np.roll(np.arange(num_qbits), np.random.randint(num_qbits)), split_indices) for _ in range(n)]
 
 
 def n_random_gas_orders(num_qbits: int, chunk_sizes: list[int], n: int) -> list[np.ndarray]:
@@ -36,3 +37,45 @@ def n_alternating_c5_orders(num_qbits: int, n: int) -> list[np.ndarray]:
     a = np.roll(a, 1, axis=1)
     possible_order_2 = np.array([(a[:, i: i + 2]).flatten() for i in range(0, num_qbits // 2, 2)])
     return [possible_order_1 if i % 2 == 0 else possible_order_2 for i in range(n)]
+
+
+def n_random_c5_orders(num_qbits: int, n: int) -> list[np.ndarray]:
+    assert num_qbits % 4 == 0, "n must be divisible by 4"
+    assert num_qbits >= 8, "n must be at least 8"
+    a = np.arange(num_qbits).reshape(2, num_qbits // 2)
+    possible_order_1 = np.array([(a[:, i: i + 2]).flatten() for i in range(0, num_qbits // 2, 2)])
+    a = np.roll(a, 1, axis=1)
+    possible_order_2 = np.array([(a[:, i: i + 2]).flatten() for i in range(0, num_qbits // 2, 2)])
+
+    # randomly select between the two possible orders
+    return [possible_order_1 if rng.random() < 0.5 else possible_order_2 for _ in range(n)]
+
+
+def n_random_c7_orders(num_qbits: int, n: int) -> list[np.ndarray]:
+    assert num_qbits % 4 == 0, "n must be divisible by 4"
+    assert num_qbits >= 8, "n must be at least 12"
+
+    if num_qbits == 8:
+        # groups = [vv, H, _H]
+        groups = [[[0, 2, 1, 3], [4, 6, 5, 7]], [[0, 2, 4, 6], [1, 3, 5, 7]], [[4, 6, 1, 3], [0, 2, 5, 7]]]
+    elif num_qbits == 12:
+        # groups = [vvv, vH, _vH, Hv] This system doesnt include cross horiozontal terms, ive added them on the end
+        groups = [[[0, 2, 1, 3], [4, 6, 5, 7], [8, 10, 11, 9]],
+                  [[0, 2, 1, 3], [4, 6, 8, 10], [5, 7, 9, 11]],
+                  [[0, 2, 9, 11], [1, 3, 8, 10], [4, 6, 5, 7]],
+                  [[0, 2, 4, 6], [1, 3, 5, 7], [8, 10, 9, 11]],
+                  [[1, 3, 5, 7], [4, 6, 8, 10], [9, 11, 0, 2]],
+                  [[0, 2, 4, 6], [5, 7, 9, 11], [8, 10, 1, 3]]
+                  ]
+    elif num_qbits == 16:
+        # groups = [vvvv, vvH, vHv, Hvv, _vvH, HH, _HH]
+        groups = [[[0, 2, 1, 3], [4, 6, 5, 7], [8, 10, 9, 11], [12, 14, 13, 15]],
+                  [[0, 2, 1, 3], [4, 6, 5, 7], [8, 10, 12, 14], [9, 11, 13, 15]],
+                  [[0, 2, 1, 3], [4, 6, 8, 10], [5, 7, 9, 11], [12, 14, 13, 15]],
+                  [[0, 2, 4, 6], [1, 3, 5, 7], [8, 10, 9, 11], [12, 14, 13, 15]],
+                  [[0, 2, 13, 15], [1, 3, 12, 14], [4, 6, 5, 7], [8, 10, 9, 11]],
+                  [[0, 2, 4, 6], [1, 3, 5, 7], [8, 10, 12, 14], [9, 11, 13, 15]],
+                  [[0, 2, 13, 15], [1, 3, 12, 14], [4, 6, 8, 10], [5, 7, 9, 11]]
+                  ]
+
+    return [rng.choice(groups) for _ in range(n)]
