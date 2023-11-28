91d6078b3a06fd21af0ca3c1cbb17902d03847ae
Jackson Henry <jackson.henry3@gmail.com>
2023-11-28 14:38:58 -0500

    generating standardized orders and unitaries

---
+++ simulation_configuration/order_generator.py
@@ -0,0 +1,27 @@
+# Add directory above current directory to path
+import sys
+import numpy as np
+import os
+
+sys.path.insert(0, '..')
+
+from src import orders
+
+# generate random orders and save them in simulation_configuration/ for each number of qbits from 8,12,and 16
+
+num_iterations = 1000
+for num_qbits in [8, 12, 16]:
+    gas_orderings = orders.n_random_gas_orders(num_qbits=num_qbits, n=num_iterations)
+    c5_orderings = orders.n_random_c5_orders(num_qbits=num_qbits, n=num_iterations)
+    c6_orderings = orders.n_random_c6_orders(num_qbits=num_qbits, n=num_iterations)
+    c7_orderings = orders.n_random_c7_orders(num_qbits=num_qbits, n=num_iterations)
+    messenger_orderings = orders.n_alternating_messenger_orders(num_qbits=num_qbits, n=num_iterations)
+
+    # use numpy to save the orders as npy files so they can be easily read as numpy arrays
+    base_path = f"../simulation_configuration/{num_qbits}qbits/orders/"
+
+    np.save(base_path + "gas_1.npy", gas_orderings) if not os.path.exists(base_path + "gas_1.npy") else print(f"path already exists, skipping gas {num_qbits} qbits")
+    np.save(base_path + "c5_1.npy", c5_orderings) if not os.path.exists(base_path + "c5_1.npy") else print(f"path already exists, skipping c5 {num_qbits} qbits")
+    np.save(base_path + "c6_1.npy", c6_orderings) if not os.path.exists(base_path + "c6_1.npy") else print(f"path already exists, skipping c6 {num_qbits} qbits")
+    np.save(base_path + "c7_1.npy", c7_orderings) if not os.path.exists(base_path + "c7_1.npy") else print(f"path already exists, skipping c7 {num_qbits} qbits")
+    np.save(base_path + "messenger_1.npy", messenger_orderings) if not os.path.exists(base_path + "messenger_1.npy") else print(f"path already exists, skipping messenger {num_qbits} qbits")
