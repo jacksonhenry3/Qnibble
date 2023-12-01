e49bb61e1d15887d6b1da562567e9ee107ba7781
Jackson Henry <jackson.henry3@gmail.com>
2023-12-01 09:01:55 -0500

    remove simulation configuration and add seeds to random generation

--- src/orders.py
+++ src/orders.py
@@ -1,12 +1,11 @@
 import numpy as np
 import numpy.random
 
-rng = np.random.default_rng()
 
-
-def n_random_c5_orders(num_qbits: int, n: int) -> list[np.ndarray]:
+def n_random_c5_orders(num_qbits: int, n: int, seed=None) -> list[np.ndarray]:
     assert num_qbits % 4 == 0, "n must be divisible by 4"
     assert num_qbits >= 8, "n must be at least 8"
+    rng = np.random.default_rng(seed)
     a = np.arange(num_qbits).reshape(2, num_qbits // 2)
     possible_order_1 = np.array([(a[:, i: i + 2]).flatten() for i in range(0, num_qbits // 2, 2)])
     a = np.roll(a, 1, axis=1)
@@ -20,22 +19,24 @@ def n_random_c5_orders(num_qbits: int, n: int) -> list[np.ndarray]:
     return result
 
 
-def n_random_c6_orders(num_qbits: int, n: int) -> list[np.ndarray]:
+def n_random_c6_orders(num_qbits: int, n: int, seed=None) -> list[np.ndarray]:
     assert num_qbits % 4 == 0, "n must be divisible by 4"
+    rng = np.random.default_rng(seed)
     split_indices = [4 * i for i in range(1, num_qbits // 4)]
 
     result = []
     for _ in range(n):
         order = np.split(np.roll(np.arange(num_qbits), np.random.randint(num_qbits)), split_indices)
-        permuted_order = [np.random.permutation(chunk) for chunk in order]
+        permuted_order = [rng.permutation(chunk) for chunk in order]
         result.append(np.array(permuted_order))
 
     return result
 
 
-def n_random_c7_orders(num_qbits: int, n: int) -> list[np.ndarray]:
+def n_random_c7_orders(num_qbits: int, n: int, seed=None) -> list[np.ndarray]:
     assert num_qbits % 4 == 0, "n must be divisible by 4"
     assert num_qbits >= 8, "n must be at least 12"
+    rng = np.random.default_rng(seed)
 
     if num_qbits == 8:
         # groups = [vv, H, _H]
@@ -67,18 +68,20 @@ def n_random_c7_orders(num_qbits: int, n: int) -> list[np.ndarray]:
     return result
 
 
-def n_random_gas_orders(num_qbits: int, n: int) -> list[np.ndarray]:
+def n_random_gas_orders(num_qbits: int, n: int, seed=None) -> list[np.ndarray]:
+    rng = np.random.default_rng(seed)
     assert num_qbits % 4 == 0, "n must be divisible by 4"
     split_indices = [4 * i for i in range(1, num_qbits // 4)]
     return [np.split(rng.permutation(num_qbits), split_indices) for _ in range(n)]
 
 
-def n_alternating_messenger_orders(num_qbits: int, n: int) -> list[np.ndarray]:
+def n_alternating_messenger_orders(num_qbits: int, n: int, seed=None) -> list[np.ndarray]:
     """
     The first and last qbit will be the messenger qbits
     """
 
     assert num_qbits % 4 == 0, "n must be a multiple of 4"
+    rng = np.random.default_rng(seed)
     first_order = np.arange(num_qbits).reshape((num_qbits // 4, 4))
     second_order = np.arange(num_qbits)
     second_order[[0, -1]] = second_order[[-1, 0]]
