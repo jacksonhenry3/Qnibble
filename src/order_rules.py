f7fd80a4fc85eeaa195c535dc03579a4a38ea01a
UnnatiAkhouri <94016784+UnnatiAkhouri@users.noreply.github.com>
2024-11-05 15:15:27 -0500

    greedy, mimic landmax all 3 checked with notes

--- src/order_rules.py
+++ src/order_rules.py
@@ -66,7 +66,8 @@ def greedy(past_order, prev_pops, pops, two_qubit_dms_previous, two_qubit_dms_cu
         sub_unitary: the unitary that operates on a group
         dm: the current density matrix
     """
-
+    #convert pops to a list otherwise it is a dictionary
+    pops = list(pops.values())
     num_qubits = len(pops)
     chunk_size = 2
 
@@ -93,9 +94,14 @@ def greedy(past_order, prev_pops, pops, two_qubit_dms_previous, two_qubit_dms_cu
 
     all_qubits = set([i for i in range(num_qubits)])
 
+    print(f"all_orders: {all_orders}")  # Check if all_orders is populated
+
     qpopth = (1/num_qubits)*sum(pops)
+    print(f"thermal Q pop: {qpopth}")  # Check if all_orders is populated
+
     score_board = []
     for order in all_orders:
+        print(f"Processing order: {order}")
         dist = []
         pops_of_updated_sub_dm = []
         order=order.tolist()
@@ -106,10 +112,14 @@ def greedy(past_order, prev_pops, pops, two_qubit_dms_previous, two_qubit_dms_cu
             updated_sub_dm = sub_unitary * sub_dm * sub_unitary.H
             pops_of_updated_sub_dm.append(measure.pops(updated_sub_dm))
         pops_of_updated_sub_dm = np.array(pops_of_updated_sub_dm).flatten()
+        print(f"Current populations: {pops}")
+        print(f"Updated populations: {pops_of_updated_sub_dm}")
         for qpop in pops_of_updated_sub_dm:
             dist.append(abs(qpop - qpopth))
         score_card = [order, sum(dist)]
+        print(f"Adding score card: {score_card}")
         score_board.append(score_card)
+        print(f"Final score card: {score_card}")
 
     current_max_score = 0
     current_order = None
@@ -119,6 +129,9 @@ def greedy(past_order, prev_pops, pops, two_qubit_dms_previous, two_qubit_dms_cu
             current_order = order
     if current_order is None:
         raise ValueError("score_board was empty; no order selected")
+
+    print(f"Final Order: {current_order}")
+
     return current_order
 
 
@@ -272,8 +285,8 @@ def landscape_maximizes(past_order, prev_pops, pops, two_qubit_dms_previous, two
 
     chunk_size = 2
     #convert pops into a list. They way its stored, pops is a dicitonary!!
+    pops = list(pops.values())
     num_qubits = len(pops)
-    pops =list(pops.values())
     #print(f"Length of pops: {pops}")
 
     match connectivity:
@@ -376,6 +389,12 @@ def mimic(past_order, prev_pops, pops, two_qubit_dms_previous, two_qubit_dms_cur
             sub_unitary: the unitary that operates on a group
             dm: the current density matrix
         """
+        # convert pops into a list. They way its stored, pops is a dicitonary!!
+        pops = list(pops.values())
+        prev_pops = list(prev_pops.values())
+        print(f"Current pops: {pops}")
+        print(f"Prev pops: {prev_pops}")
+
         num_qubits = len(pops)
         chunk_size = 2
 
@@ -402,7 +421,7 @@ def mimic(past_order, prev_pops, pops, two_qubit_dms_previous, two_qubit_dms_cur
 
         all_qubits = set([i for i in range(num_qubits)])
 
-        # find all neighbours of a qubit WORKING
+        # find all neighbours of a qubit
         def find_shared_elements(list_of_lists_of_lists, target_element):
             shared_elements = set()  # Use a set to store unique elements
             for sublist in list_of_lists_of_lists:
@@ -411,20 +430,28 @@ def mimic(past_order, prev_pops, pops, two_qubit_dms_previous, two_qubit_dms_cur
                         shared_elements.update([elem for elem in subsublist if elem != target_element])
             return list(shared_elements)
 
-        # WORKING: gives a list of lists of lists. Each list inside the list is the ordered according q index and corresponds to the lists of neighbours that qubit index has
+        #Gives a list of lists of lists. Each list inside the list is the ordered according q index and corresponds to the lists of neighbours that qubit index has
         # list of lists where each list is the set of neighbours of the qubit with that list index. ie the nth list is a list of neighbours of the nth qubit on landscape
         neighbours_qubit_index = []
         for id in range(num_qubits):
             neighbours_qubit_index.append(find_shared_elements(all_orders, id))
+        print(f"List of neighbours per Q: {neighbours_qubit_index}")
+
 
         extractable_work_i0 = np.array(
             measure.extractable_work_of_each_qubit_from_pops(prev_pops))
         extractable_work_i1 = np.array(
             measure.extractable_work_of_each_qubit_from_pops(pops))
+
+        print(f"Ext Work prev step: {extractable_work_i0}")
+        print(f"Current Ext Work: {extractable_work_i1}")
+
         change_in_ex_work_prev_step = extractable_work_i1 - extractable_work_i0
+        print(f"Change in ext work at current step: {change_in_ex_work_prev_step}")
+
 
         # code to find the qubit the target qubit was paired in the previous order
-        # WORKING: returns a qubit index corresponding to the  past orders groupings
+        # Returns a qubit index corresponding to the  past orders groupings
         def paired_element(given_element, past_order):
             for pair in past_order:
                 # Convert the NumPy array to a list
@@ -436,7 +463,7 @@ def mimic(past_order, prev_pops, pops, two_qubit_dms_previous, two_qubit_dms_cur
             return None
 
         # code to find the qubit indices with change in extractable work in ascending order. These will help find the decider qubits
-        # WORKING: returns a list of indices who have the corresponding value given in the argument in ascending order
+        # Returns a list of indices who have the corresponding value given in the argument in ascending order
         def find_indices_in_ascending_order(values):
             # Enumerate the values to get (index, value) pairs
             indexed_values = list(enumerate(values))
@@ -450,7 +477,7 @@ def mimic(past_order, prev_pops, pops, two_qubit_dms_previous, two_qubit_dms_cur
             return indices_in_ascending_order
 
         decider_Q_index = find_indices_in_ascending_order(change_in_ex_work_prev_step)
-        # print(decider_Q_index)
+        print(f"Decider Q to start mimic: {decider_Q_index}")
 
         # code to find allowed neighbourhoods after some have been fixed
 
@@ -476,11 +503,13 @@ def mimic(past_order, prev_pops, pops, two_qubit_dms_previous, two_qubit_dms_cur
             return matching_lists
 
         for qubit_id in decider_Q_index:
+            print(f"Qubit id in loop: {qubit_id}")
             # we start with the qubits in ascending order
             # we check that the qubit in loop does not already exist in the pairings made previously; this would not matter for the first qubit but can hinder later on
             fullorder = np.array(order).flatten()
-            # print(fullorder)
+            print(f"Full order: {fullorder}")
             if qubit_id not in fullorder:
+                print(f"Qubit id not in loop: {qubit_id}")
                 # we proceed only if the qubit id does not already exists in the ordering
                 # print(qubit_id)
                 sub_order = []
@@ -492,40 +521,51 @@ def mimic(past_order, prev_pops, pops, two_qubit_dms_previous, two_qubit_dms_cur
                     # finds the subset of full network ordering for which the subset of pairs match
                     # print(match)
                     allowed_orders = match
+                    print(f"allowed orders: {allowed_orders}")
                 else:
                     allowed_orders = all_orders
                 if len(allowed_orders) == 1:
+                    print(f"Was length equal to 1 yes!")
                     current_order = allowed_orders[0]
                     # print("this happened")
                     # if only one ordering is allowed then no need to do further optimization. This is the order of choice set by other qubits and connectivity
-                    return current_order
+                    print(f"Current order : {current_order}")
+                    #return current_order
                 else:
                     # for id in range(num_qubits):
                     # neighbours_qubit_index.append(find_shared_elements(allowed_orders, id))
                     neighbours = find_shared_elements(allowed_orders, qubit_id)
+                    print(f"Neighbours : {neighbours}")
                     # neighbours = neighbours_qubit_index[qubit_id]
                     # neighbours = [x for x in neighbours if x not in np.array(order).flatten()]
                     # print(neighbours)
                     pop_diff_to_mimic = 0
                     for neighbour in neighbours:
+                        print(f"neighbour currently checking : {neighbour}")
                         if max_D_W_ex <= change_in_ex_work_prev_step[neighbour]:
                             max_D_W_ex = change_in_ex_work_prev_step[neighbour]
                             neighbour_to_mimic = neighbour
                             pop_diff_to_mimic = prev_pops[neighbour_to_mimic] - prev_pops[
                                 paired_element(neighbour_to_mimic, past_order)]
-                    # print(neighbour_to_mimic)
+                    print(f"neighbour to mimic: {neighbour_to_mimic}")
                     diff = 0
                     min_diff = float('inf')
                     for neighbour in neighbours:
                         pop_diff = pops[qubit_id] - pops[neighbour]
                         diff_with_n = pop_diff - pop_diff_to_mimic
+                        print(f"pop diff: {pop_diff}")
+                        print(f"compared to pop diff to mimic: {diff_with_n}")
                         if diff_with_n <= min_diff:
                             Q_pair = neighbour
                             min_diff=diff_with_n
+                    print(f"Neighbour getting paired with: {Q_pair}")
                     sub_order = [qubit_id, Q_pair]
+                    print(f"Sub order : {sub_order}")
                     order.append(sub_order)
                     # print(order)
                     current_order = order
+
+        print(f"Current order : {current_order}")
         return current_order
 
 
